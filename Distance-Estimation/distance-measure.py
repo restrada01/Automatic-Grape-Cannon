cfbacdb1033fccb16096f9301ebaafe5b2cb615e
Rafael <restrada@uwo.ca>
2022-07-14 22:11:09 -0400

    reference images

--- Distance-Estimation/distance-measure.py
+++ Distance-Estimation/distance-measure.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 
 # VARIABLES
 # measured face width, in cm's
@@ -10,17 +11,45 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 # for formatting rectangles displayed
 WHITE = (255, 255, 255)
 STROKE = 2
+# paths for reference images
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+image_dir = os.path.join(BASE_DIR, "referenceImages")
 
 # function to draw rectangles on detected faces and return width of the face
-def getFaceData(frame):
-    face_width = 0
+def getFaceWidth(frame):
+    faceWidth = 0
     grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)   # convert frame to greyscale for facial detection
     faces = face_cascade.detectMultiScale(grey, scaleFactor=1.3, minNeighbors=5)
     # draw rectangles on frame image
     for (x, y, w, h) in faces: 
         cv2.rectangle(frame, (x, y), (x+w, y+h), WHITE, STROKE) 
-        face_width = w
-    return face_width
+        faceWidth = w
+    return faceWidth
+
+# function to measure the lens' focal length (Similar Triangles)
+# this only uses readings from one measured image. To get more accurate, will help to take average of several measurements
+def getFocalLength(measuredWidth):
+    focalLength = (measuredWidth * KNOWN_DISTANCE) / KNOWN_WIDTH
+    return focalLength
+
+# function to return the estimated distance of object according to the maesured width
+def getDistance(measuredWidth, focalLength):
+    distance = (focalLength * KNOWN_WIDTH) / measuredWidth
+    return distance
+
+# for every  reference image from directory
+#for root, dirs, files in os.walk(image_dir): 
+ #   for file in files:
+  #         path = os.path.join(root, file)
+   #         print(path)
+refImage = cv2.imread('1.jpg', 0)
+print(refImage)
+
+
+
+# train model to detect pixels for known width, and find focal length
+
+
 
 
 cap = cv2.VideoCapture(0)
@@ -28,10 +57,8 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
 
-    detectedFaceWidth = getFaceData(frame)
-    print(detectedFaceWidth)
-    
-    cv2.imshow('frame', frame)
+    measuredWidth = getFaceWidth(frame)
+    cv2.imshow('refImage', refImage)
     if cv2.waitKey(1) == ord('q'):
         break
 
